bdea8ed7cdf33bcd8aba7ff8eb33f2ab8aac2db8
MayaIw <100341938+MayaIw@users.noreply.github.com>
2024-09-02 21:09:11 +0300

    removed id

--- server/models/AttractionModel.js
+++ server/models/AttractionModel.js
@@ -4,10 +4,6 @@ const Schema = mongoose.Schema;
 
 const attractionSchema = new Schema(
   {
-    id: {
-      type: String,
-      required: true,
-    },
     title: {
       type: String,
       required: true,
