84e20e829e3256baf55590750012b2237efe35b9
MayaIw <mayaiwanir@mail.tau.ac.il>
2024-09-07 19:29:57 +0300

    created a user registration portion

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const rubberDucksRoutes = require('./routes/rubberDucks')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const rubberDucksRoutes = require("./routes/rubberDucks");
+const usersRoutes = require("./routes/users");
 
 dotenv.config();
 
@@ -13,30 +14,31 @@ const PORT = process.env.PORT;
 const app = express();
 
 // Middleware
-app.use(express.json())
-app.use(cors({
-  origin: process.env.CLIENT_URL
-}));
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 
 app.use((req, res, next) => {
-  console.log(req.path, req.method)
-  next()
-})
+  console.log(req.path, req.method);
+  next();
+});
 
 // Routes
-app.use('/api/rubberDucks', rubberDucksRoutes)
+app.use("/api/rubberDucks", rubberDucksRoutes);
+app.use("/api/users", usersRoutes);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(PORT, () => {
-      console.log('connected to mongoDB & listening on port', process.env.PORT)
-    })
-  }).catch((err) => {
-    console.log(err)
+      console.log("connected to mongoDB & listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-
-
-
