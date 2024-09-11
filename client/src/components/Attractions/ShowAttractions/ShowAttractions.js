5cf1af741c3bd5cad0bb7a5d98660570112d39d5
MayaIw <mayaiwanir@mail.tau.ac.il>
2024-09-11 16:48:03 +0300

    testing
    added password check, and updated userName and profilePic after successful login

--- client/src/components/Attractions/ShowAttractions/ShowAttractions.js
+++ client/src/components/Attractions/ShowAttractions/ShowAttractions.js
@@ -1,36 +1,38 @@
-import React, { useEffect, useState, useContext } from 'react';
-import styles from './ShowAttractions.module.css'; 
-import styles_pagination from '../../Pagination.module.css'; 
-import { AttractionContext } from '../../../context/AttractionContext';
-import SingleAttraction from '../SingleAttraction/SingleAttraction'; 
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import React, { useEffect, useState, useContext } from "react";
+import styles from "./ShowAttractions.module.css";
+import styles_pagination from "../../Pagination.module.css";
+import { AttractionContext } from "../../../context/AttractionContext";
+import SingleAttraction from "../SingleAttraction/SingleAttraction";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 // present the attraction in home page, according to the filters
 const ShowAttractions = () => {
-  const {filterValuesAttractions, getAttractions } = useContext(AttractionContext);
+  const { filterValuesAttractions, getAttractions } =
+    useContext(AttractionContext);
   const [loading, setLoading] = useState(true);
-  
-  // pagination: 
+
+  // pagination:
   let currAttractions = getAttractions();
   const [currentPage, setCurrentPage] = useState(1);
   const [recordsPerPage] = useState(1);
   const indexOfLastRecord = currentPage * recordsPerPage;
   const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
   // Records to be displayed on the current page
-  const currentRecords = currAttractions.slice(indexOfFirstRecord, 
-                                    indexOfLastRecord);
+  const currentRecords = currAttractions.slice(
+    indexOfFirstRecord,
+    indexOfLastRecord
+  );
   // console.log('currentRecords changed ' + JSON.stringify(currAttractions, null, 2));
-  const nPages = Math.ceil(currAttractions.length / recordsPerPage)
+  const nPages = Math.ceil(currAttractions.length / recordsPerPage);
 
   useEffect(() => {
-    
     const fetchAttractions = async () => {
       try {
         setLoading(true); // Start loading
         currAttractions = getAttractions();
       } catch (error) {
-        console.error('Error fetching attractions:', error);
+        console.error("Error fetching attractions:", error);
       } finally {
         setLoading(false); // Stop loading
       }
@@ -46,20 +48,21 @@ const ShowAttractions = () => {
   if (loading) {
     return <div>Loading attractions...</div>; // Display loading message
   }
-  if (!currAttractions || currAttractions.length === 0) return <div>No attractions available</div>;
+  if (!currAttractions || currAttractions.length === 0)
+    return <div>No attractions available</div>;
 
   return (
     <div>
       <div className={styles.container}>
         {currentRecords.map((attraction) => (
-            <SingleAttraction key={attraction._id} attraction={attraction} />
-          ))}
+          <SingleAttraction key={attraction._id} attraction={attraction} />
+        ))}
       </div>
       <div className={styles_pagination.paginationWrapper}>
         <Stack spacing={2}>
           <Pagination
             count={nPages}
-            variant="outlined" 
+            variant="outlined"
             shape="rounded"
             page={currentPage}
             onChange={handleChange}
@@ -70,6 +73,5 @@ const ShowAttractions = () => {
     </div>
   );
 };
-
 
 export default ShowAttractions;
--- client/src/context/AttractionContext.js
+++ client/src/context/AttractionContext.js
@@ -1,46 +1,56 @@
-import React, { createContext, useEffect, useState } from 'react';
-import {fetchAttractions} from '../services/utils';
+import React, { createContext, useEffect, useState } from "react";
+import { fetchAttractions } from "../services/utils";
 const AttractionContext = createContext();
 
 const AttractionProvider = ({ children }) => {
-    const [attractions, setAttractions] = useState([]);
-    const [filterValuesAttractions, setFilterValuesAttractions] = useState({});
+  const [attractions, setAttractions] = useState([]);
+  const [filterValuesAttractions, setFilterValuesAttractions] = useState({});
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await fetchAttractions();
-                setAttractions(data);
-            } catch (error) {
-            console.error('Error fetching attractions: ', error);
-            }
-        };
-        fetchData();
-    }, []);
-    
-    const getAttractions = () => {
-        const res = attractions.filter((attraction) => {
-            const { continents = [], categories = [], title = '' } = filterValuesAttractions;
-
-            if (continents.length === 0 && categories.length === 0) {
-                return attraction.title.toLowerCase().includes(title.toLowerCase());
-            } 
-            else {
-                return (
-                    (!continents.length || continents.includes(attraction.continent)) &&
-                    (!categories.length || categories.includes(attraction.category)) &&
-                    attraction.title.toLowerCase().includes(title.toLowerCase())
-                );
-            }
-        });
-
-        return res;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchAttractions();
+        setAttractions(data);
+      } catch (error) {
+        console.error("Error fetching attractions: ", error);
+      }
     };
+    fetchData();
+  }, []);
 
-    return (
-        <AttractionContext.Provider value={{ attractions, filterValuesAttractions, setFilterValuesAttractions, getAttractions }}>
-            {children}
-        </AttractionContext.Provider>
-    );
+  const getAttractions = () => {
+    const res = attractions.filter((attraction) => {
+      const {
+        continents = [],
+        categories = [],
+        title = "",
+      } = filterValuesAttractions;
+
+      if (continents.length === 0 && categories.length === 0) {
+        return attraction.title.toLowerCase().includes(title.toLowerCase());
+      } else {
+        return (
+          (!continents.length || continents.includes(attraction.continent)) &&
+          (!categories.length || categories.includes(attraction.category)) &&
+          attraction.title.toLowerCase().includes(title.toLowerCase())
+        );
+      }
+    });
+
+    return res;
+  };
+
+  return (
+    <AttractionContext.Provider
+      value={{
+        attractions,
+        filterValuesAttractions,
+        setFilterValuesAttractions,
+        getAttractions,
+      }}
+    >
+      {children}
+    </AttractionContext.Provider>
+  );
 };
 export { AttractionContext, AttractionProvider };
