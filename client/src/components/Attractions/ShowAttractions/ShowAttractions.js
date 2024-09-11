afc80c98313642151fd43d2e9e1a6663663e5cb8
MayaIw <mayaiwanir@mail.tau.ac.il>
2024-09-11 16:48:03 +0300

    testing

--- client/src/components/Attractions/ShowAttractions/ShowAttractions.js
+++ client/src/components/Attractions/ShowAttractions/ShowAttractions.js
@@ -1,29 +1,30 @@
-import React, { useEffect, useState, useContext } from 'react';
-import styles from './ShowAttractions.module.css'; 
-import styles_pagination from '../../Pagination.module.css'; 
-import { AttractionContext } from '../../../context/AttractionContext';
-import SingleAttraction from '../SingleAttraction/SingleAttraction'; 
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import React, { useEffect, useState, useContext } from "react";
+import styles from "./ShowAttractions.module.css";
+import styles_pagination from "../../Pagination.module.css";
+import { AttractionContext } from "../../../context/AttractionContext";
+import SingleAttraction from "../SingleAttraction/SingleAttraction";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 // present the attraction in home page, according to the filters
 const ShowAttractions = ({ continent, category }) => {
   const { attractions, filterAttractions } = useContext(AttractionContext);
   const [loading, setLoading] = useState(true);
-  const [ attractionsChanged, setAttractionsChanged] = useState(true)
-  
+  const [attractionsChanged, setAttractionsChanged] = useState(true);
+
   // pagination:
   const [currentPage, setCurrentPage] = useState(1);
   const [recordsPerPage] = useState(1);
   const indexOfLastRecord = currentPage * recordsPerPage;
   const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
   // Records to be displayed on the current page
-  const currentRecords = attractions.slice(indexOfFirstRecord, 
-                                    indexOfLastRecord);
-  const nPages = Math.ceil(attractions.length / recordsPerPage)
+  const currentRecords = attractions.slice(
+    indexOfFirstRecord,
+    indexOfLastRecord
+  );
+  const nPages = Math.ceil(attractions.length / recordsPerPage);
 
   useEffect(() => {
-    
     const fetchAttractions = async () => {
       try {
         setLoading(true); // Start loading
@@ -33,11 +34,11 @@ const ShowAttractions = ({ continent, category }) => {
         } else {
           // Fetch with filters
           await filterAttractions(continent, category);
-          setAttractionsChanged(false)
+          setAttractionsChanged(false);
           setTimeout(() => setAttractionsChanged(true), 1);
         }
       } catch (error) {
-        console.error('Error fetching attractions:', error);
+        console.error("Error fetching attractions:", error);
       } finally {
         setLoading(false); // Stop loading
       }
@@ -45,7 +46,7 @@ const ShowAttractions = ({ continent, category }) => {
 
     fetchAttractions();
   }, [continent, category]); //continent, category, filterAttractions
-  
+
   const handleChange = (event, value) => {
     setCurrentPage(value);
   };
@@ -53,22 +54,22 @@ const ShowAttractions = ({ continent, category }) => {
   if (loading) {
     return <div>Loading attractions...</div>; // Display loading message
   }
-  if (!attractions || attractions.length === 0) return <div>No attractions available</div>;
+  if (!attractions || attractions.length === 0)
+    return <div>No attractions available</div>;
 
   return (
     <div>
       <div className={styles.container}>
-        {attractionsChanged && (
-        currentRecords.map((attraction) => (
+        {attractionsChanged &&
+          currentRecords.map((attraction) => (
             <SingleAttraction key={attraction._id} attraction={attraction} />
-          ))
-        )}
+          ))}
       </div>
       <div className={styles_pagination.paginationWrapper}>
         <Stack spacing={2}>
           <Pagination
             count={nPages}
-            variant="outlined" 
+            variant="outlined"
             shape="rounded"
             page={currentPage}
             onChange={handleChange}
@@ -79,6 +80,5 @@ const ShowAttractions = ({ continent, category }) => {
     </div>
   );
 };
-
 
 export default ShowAttractions;
