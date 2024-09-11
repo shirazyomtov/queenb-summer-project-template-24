45918491d716f5a2d0fa8721b0e0202bc1b06e5f
MayaIw <mayaiwanir@mail.tau.ac.il>
2024-09-11 16:48:03 +0300

    testing

--- client/src/components/Attractions/SingleAttraction/SingleAttraction.js
+++ client/src/components/Attractions/SingleAttraction/SingleAttraction.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import styles from '../ShowAttractions/ShowAttractions.module.css'; // Adjust the path if needed
+import React from "react";
+import styles from "../ShowAttractions/ShowAttractions.module.css"; // Adjust the path if needed
 
 const SingleAttraction = ({ attraction }) => {
   if (!attraction) return null;
 
   return (
     <div className={styles.attraction}>
-        <div className={styles.attraction}>
-
-          <a href="your-link.html" className={styles.attraction_link}>
-            {attraction.imageUrl && (
-              <img
-                src={attraction.imageUrl}
-                alt={attraction.title}
-                className={styles.img}
-              />
-            )}
-            <h2 className={styles.attractionName}>{attraction.title}</h2>
-            <p className={styles.attractionPlace}>{attraction.city}, {attraction.country}</p>
-          </a>
+      <div className={styles.attraction}>
+        <a href="your-link.html" className={styles.attraction_link}>
+          {attraction.imageUrl && (
+            <img
+              src={attraction.imageUrl}
+              alt={attraction.title}
+              className={styles.img}
+            />
+          )}
+          <h2 className={styles.attractionName}>{attraction.title}</h2>
+          <p className={styles.attractionPlace}>
+            {attraction.city}, {attraction.country}
+          </p>
+        </a>
       </div>
     </div>
   );
--- client/src/context/AttractionContext.js
+++ client/src/context/AttractionContext.js
@@ -1,32 +1,32 @@
-import React, { createContext, useState } from 'react';
-import api from '../services/api';
-import qs from 'qs';
+import React, { createContext, useState } from "react";
+import api from "../services/api";
+import qs from "qs";
 
 const AttractionContext = createContext();
 
 const AttractionProvider = ({ children }) => {
-    const [attractions, setAttractions] = useState([]);
+  const [attractions, setAttractions] = useState([]);
 
-    // Function to fetch filtered attractions
-    const filterAttractions = async (cont, cate) => {
-        try {
-            // Construct the query string with parameters
-            const queryParams =  qs.stringify({
-                continent: cont , // Default to empty string if not provided
-                category: cate 
-            }, );
-            const response = await api.get(`/attractions/filter?${queryParams}`);
-            setAttractions(response.data);
-        } catch (error) {
-            console.error('Error fetching attractions:', error);
-            console.error('Error fetching attractions:', error);
-        }
-    };
+  // Function to fetch filtered attractions
+  const filterAttractions = async (cont, cate) => {
+    try {
+      // Construct the query string with parameters
+      const queryParams = qs.stringify({
+        continent: cont, // Default to empty string if not provided
+        category: cate,
+      });
+      const response = await api.get(`/attractions/filter?${queryParams}`);
+      setAttractions(response.data);
+    } catch (error) {
+      console.error("Error fetching attractions:", error);
+      console.error("Error fetching attractions:", error);
+    }
+  };
 
-    return (
-        <AttractionContext.Provider value={{ attractions, filterAttractions }}>
-            {children}
-        </AttractionContext.Provider>
-    );
+  return (
+    <AttractionContext.Provider value={{ attractions, filterAttractions }}>
+      {children}
+    </AttractionContext.Provider>
+  );
 };
 export { AttractionContext, AttractionProvider };
